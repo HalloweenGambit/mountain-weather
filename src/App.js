efb2a7bb3c67482dd789c8ad4c9841c135b79ef2
0xJariel <0xJariel@protonmail.com>
2022-10-26 16:20:42 -0600

    <First '0xJariel' commit :D >

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className="App">
+      <UilReact size="140" color="#61DAFB" />
+      <h1 className="font-bold text-blue-500">Hello thessre!</h1>
+    </div>
+  );
+}
+
+export default App;
+ 
